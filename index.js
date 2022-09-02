743baf7811e8bd3c619157bef12e9f5a676af49a
ekam02 <ekam02@gmail.com>
2022-09-02 11:10:13 -0500

    Hola mundo en Node.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.set('port', process.env.PORT || 9000);
+
+app.use(express.json());
+
+app.use('/', (req, res) => {
+    res.json({
+        message: "Hola mundo desde Heroku."
+    });
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`El servidor se desplegó desde el puerto ${app.get('port')}`);
+});
